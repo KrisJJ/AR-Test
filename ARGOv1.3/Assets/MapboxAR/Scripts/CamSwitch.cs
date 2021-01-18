683b5fff2da29e434a16b30066d377585b2c8964
Egor <legenda.raze@gmail.com>
2021-01-18 16:48:39 +1000

    New Version 1.3

---
+++ ARGOv1.3/Assets/MapboxAR/Scripts/CamSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CamSwitch : MonoBehaviour
+{
+    public GameObject VuforiaCam;
+    public GameObject MapBoxCam;
+    public GameObject MapboxPromptPanel;
+    public Text scoreText;
+
+    public void Switchcam() {
+        VuforiaCam.SetActive(true);
+        
+        MapBoxCam.SetActive(false);
+        MapboxPromptPanel.SetActive(false);
+        
+        scoreText.text = " ";
+
+    }
+
+}
---
+++ ARGOv1.3/Assets/MapboxAR/Scripts/UploadPrompts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UploadPrompts : MonoBehaviour
+{
+    public TMP_Text PromptTextObject;
+    void Start()
+    {
+        PromptTextObject.text = GameObject.Find("PromptManager").GetComponent<PromptText>().promptsList[1];
+        
+    }
+    private void Update()
+    {
+
+        int score = GameObject.Find("ScoreManager").GetComponent<ScoreManager>().score;
+        PromptTextObject.text = GameObject.Find("PromptManager").GetComponent<PromptText>().promptsList[score+1];
+    }
+
+}
